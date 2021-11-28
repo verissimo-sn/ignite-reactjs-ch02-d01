956231d33ec5b9315309aebe61cc5248e4721041
verissimo-sn <verissimo_sn@hotmail.com>
2021-11-28 09:31:28 -0300

    feat: add sumAmount product cart

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,9 +25,13 @@ const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
   const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, { id }) => {
+    return { 
+      ...sumAmount,
+      [id]: sumAmount[id] ? (sumAmount[id] + 1) : 1,
+    };
+
+  }, {} as CartItemsAmount);
 
   useEffect(() => {
     async function loadProducts() {
@@ -75,7 +79,7 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} */} 2
+              {cartItemsAmount[product.id] || 0}
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>
